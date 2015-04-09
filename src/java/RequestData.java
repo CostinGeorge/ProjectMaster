a03805f8110a34a520edc9902be38a62d7102d51
Costin Mihaita <costinmihaita@Costins-MacBook-Air.local>
2015-04-09 10:37:51 +0200

    Until now

---
+++ src/java/RequestData.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author costinmihaita
+ */
+public class RequestData {
+     
+   String  Productname ;
+
+    public String getProductname() {
+        return Productname;
+    }
+
+    public void setProductname(String Productname) {
+        this.Productname = Productname;
+    }
+    
+    
+    
+   
+    
+}
